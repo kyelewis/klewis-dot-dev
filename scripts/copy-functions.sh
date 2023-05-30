e1c434b4323ae4045724c3d669eba2d40cb9c4ae
Kye Lewis <kye.lewis@techin.site>
2023-05-30 17:45:10 +1000

    run esbuild to bundle graphweaver

--- scripts/copy-functions.sh
+++ scripts/copy-functions.sh
@@ -1,3 +1,3 @@
 rm -rf netlify
 mkdir -p netlify/functions/api
-cp -r packages/api/.graphweaver/backend/* netlify/functions/api
+pnpm esbuild --bundle ./packages/api/.graphweaver/backend --platform=node --outfile=./netlify/functions/api/index.js
